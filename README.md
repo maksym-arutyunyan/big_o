369d4c105afe222d51a3d5f10ab3fc59d295dd54
Maksym Arutyunyan <m.arutyunyan+github@gmail.com>
2022-06-16 19:40:34 +0200

    refactoring: rename best -> complexity in examples

--- README.md
+++ README.md
@@ -10,10 +10,10 @@ Infers asymptotic computational complexity.
 // f(x) = gain * x ^ 2 + offset
 let data = vec![(1., 1.), (2., 4.), (3., 9.), (4., 16.)];
 
-let (best, _all) = big_o::infer_complexity(data).unwrap();
+let (complexity, _all) = big_o::infer_complexity(data).unwrap();
 
-assert_eq!(best.name, big_o::Name::Quadratic);
-assert_eq!(best.notation, "O(n^2)");
-assert_approx_eq::assert_approx_eq!(best.params.gain.unwrap(), 1.0, 1e-6);
-assert_approx_eq::assert_approx_eq!(best.params.offset.unwrap(), 0.0, 1e-6);
+assert_eq!(complexity.name, big_o::Name::Quadratic);
+assert_eq!(complexity.notation, "O(n^2)");
+assert_approx_eq::assert_approx_eq!(complexity.params.gain.unwrap(), 1.0, 1e-6);
+assert_approx_eq::assert_approx_eq!(complexity.params.offset.unwrap(), 0.0, 1e-6);
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -21,25 +21,25 @@ pub use crate::params::Params;
 /// // f(x) = gain * x ^ 2 + offset
 /// let data = vec![(1., 1.), (2., 4.), (3., 9.), (4., 16.)];
 ///
-/// let (best, _all) = big_o::infer_complexity(data).unwrap();
+/// let (complexity, _all) = big_o::infer_complexity(data).unwrap();
 ///
-/// assert_eq!(best.name, big_o::Name::Quadratic);
-/// assert_eq!(best.notation, "O(n^2)");
-/// assert_approx_eq::assert_approx_eq!(best.params.gain.unwrap(), 1.0, 1e-6);
-/// assert_approx_eq::assert_approx_eq!(best.params.offset.unwrap(), 0.0, 1e-6);
+/// assert_eq!(complexity.name, big_o::Name::Quadratic);
+/// assert_eq!(complexity.notation, "O(n^2)");
+/// assert_approx_eq::assert_approx_eq!(complexity.params.gain.unwrap(), 1.0, 1e-6);
+/// assert_approx_eq::assert_approx_eq!(complexity.params.offset.unwrap(), 0.0, 1e-6);
 /// ```
 pub fn infer_complexity(
     data: Vec<(f64, f64)>,
 ) -> Result<(Complexity, Vec<Complexity>), &'static str> {
-    let mut fitted: Vec<Complexity> = Vec::new();
+    let mut all_fitted: Vec<Complexity> = Vec::new();
     for name in name::all_names() {
         let complexity = complexity::fit(name, data.clone())?;
         if validate::is_valid(&complexity) {
-            fitted.push(complexity);
+            all_fitted.push(complexity);
         }
     }
-    fitted.sort_by(|a, b| a.params.residuals.partial_cmp(&b.params.residuals).unwrap());
-    let best = fitted[0].clone();
+    all_fitted.sort_by(|a, b| a.params.residuals.partial_cmp(&b.params.residuals).unwrap());
+    let best_complexity = all_fitted[0].clone();
 
-    Ok((best, fitted))
+    Ok((best_complexity, all_fitted))
 }
