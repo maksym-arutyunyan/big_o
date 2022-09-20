5fb288d1068e9e9bf6da71ca04dff7cc0726264f
Maksym Arutyunyan <107496615+maksym-arutyunyan@users.noreply.github.com>
2022-09-20 13:33:16 +0200

    recalculate delinearized residuals (#7)

--- src/complexity.rs
+++ src/complexity.rs
@@ -19,37 +19,35 @@ pub struct Complexity {
     pub rank: u32,
 }
 
-impl Complexity {
-    /// Returns a calculated approximation function `f(x)`
-    pub fn get_function(&self) -> Result<Box<dyn Fn(f64) -> f64>, &'static str> {
-        let p = &self.params;
-        if let (Some(a), Some(b)) = match self.name {
-            Name::Polynomial => (p.gain, p.power),
-            Name::Exponential => (p.gain, p.base),
-            _other => (p.gain, p.offset),
-        } {
-            let f: Box<dyn Fn(f64) -> f64> = match self.name {
-                Name::Constant => Box::new(move |_x| b),
-                Name::Logarithmic => Box::new(move |x| a * x.ln() + b),
-                Name::Linear => Box::new(move |x| a * x + b),
-                Name::Linearithmic => Box::new(move |x| a * x * x.ln() + b),
-                Name::Quadratic => Box::new(move |x| a * x.powi(2) + b),
-                Name::Cubic => Box::new(move |x| a * x.powi(3) + b),
-                Name::Polynomial => Box::new(move |x| a * x.powf(b)),
-                Name::Exponential => Box::new(move |x| a * b.powf(x)),
-            };
-            Ok(f)
-        } else {
-            Err("No cofficients to compute f(x)")
-        }
+/// Returns a calculated approximation function `f(x)`
+fn get_function(name: Name, params: Params) -> Result<Box<dyn Fn(f64) -> f64>, &'static str> {
+    if let (Some(a), Some(b)) = match name {
+        Name::Polynomial => (params.gain, params.power),
+        Name::Exponential => (params.gain, params.base),
+        _other => (params.gain, params.offset),
+    } {
+        let f: Box<dyn Fn(f64) -> f64> = match name {
+            Name::Constant => Box::new(move |_x| b),
+            Name::Logarithmic => Box::new(move |x| a * x.ln() + b),
+            Name::Linear => Box::new(move |x| a * x + b),
+            Name::Linearithmic => Box::new(move |x| a * x * x.ln() + b),
+            Name::Quadratic => Box::new(move |x| a * x.powi(2) + b),
+            Name::Cubic => Box::new(move |x| a * x.powi(3) + b),
+            Name::Polynomial => Box::new(move |x| a * x.powf(b)),
+            Name::Exponential => Box::new(move |x| a * b.powf(x)),
+        };
+        Ok(f)
+    } else {
+        Err("No cofficients to compute f(x)")
     }
+}
 
-    /// Computes values of `f(x)` given `x`
-    pub fn compute_f(&self, x: Vec<f64>) -> Result<Vec<f64>, &'static str> {
-        let f = self.get_function()?;
-        let y = x.into_iter().map(f).collect();
-        Ok(y)
-    }
+/// Computes values of `f(x)` given `x`
+#[allow(dead_code)]
+fn compute_f(name: Name, params: Params, x: Vec<f64>) -> Result<Vec<f64>, &'static str> {
+    let f = get_function(name, params)?;
+    let y = x.into_iter().map(f).collect();
+    Ok(y)
 }
 
 pub struct ComplexityBuilder {
@@ -62,7 +60,7 @@ impl ComplexityBuilder {
         Self { name, params: None }
     }
 
-    #[allow(dead_code)]  // Used in tests.
+    #[allow(dead_code)] // Used in tests.
     pub fn power(&mut self, x: f64) -> &mut Self {
         self.params = Some(Params::new().power(x).build());
         self
@@ -98,7 +96,7 @@ fn linearize(name: Name, x: f64, y: f64) -> (f64, f64) {
 }
 
 /// Converts linear coeffs `gain` and `offset` to corresponding complexity params.
-fn delinearize(name: Name, gain: f64, offset: f64, residuals: f64) -> Params {
+fn delinearize(name: Name, gain: f64, offset: f64) -> Params {
     // Delinearize coeffs.
     let (a, b) = match name {
         Name::Polynomial => (offset.exp(), gain),
@@ -107,10 +105,21 @@ fn delinearize(name: Name, gain: f64, offset: f64, residuals: f64) -> Params {
     };
     // Convert coeffs to params.
     match name {
-        Name::Polynomial => Params::new().gain(a).power(b).residuals(residuals).build(),
-        Name::Exponential => Params::new().gain(a).base(b).residuals(residuals).build(),
-        _other => Params::new().gain(a).offset(b).residuals(residuals).build(),
+        Name::Polynomial => Params::new().gain(a).power(b).build(),
+        Name::Exponential => Params::new().gain(a).base(b).build(),
+        _other => Params::new().gain(a).offset(b).build(),
     }
+}
+
+fn calculate_residuals(
+    name: Name,
+    params: Params,
+    data: Vec<(f64, f64)>,
+) -> Result<f64, &'static str> {
+    let f = get_function(name, params)?;
+    let residuals = data.into_iter().map(|(x, y)| (y - f(x)).abs()).sum();
+
+    Ok(residuals)
 }
 
 fn rank(name: Name, params: Params) -> u32 {
@@ -132,12 +141,10 @@ fn rank(name: Name, params: Params) -> u32 {
         Name::Linearithmic => 1_130,
         Name::Quadratic => 2_000,
         Name::Cubic => 3_000,
-        Name::Polynomial => {
-            match params.power {
-                Some(power) => std::cmp::min((1_000.0 * power) as u32, 1_000_000),
-                None => panic!("Polynomial is missing its power parameter"),
-            }
-        }
+        Name::Polynomial => match params.power {
+            Some(power) => std::cmp::min((1_000.0 * power) as u32, 1_000_000),
+            None => panic!("Polynomial is missing its power parameter"),
+        },
         Name::Exponential => 1_000_000,
     }
 }
@@ -145,12 +152,19 @@ fn rank(name: Name, params: Params) -> u32 {
 /// Fits a function of given complexity into input data.
 pub fn fit(name: Name, data: Vec<(f64, f64)>) -> Result<Complexity, &'static str> {
     let linearized = data
+        .clone()
         .into_iter()
         .map(|(x, y)| linearize(name, x, y))
         .collect();
 
-    let (gain, offset, residuals) = linalg::fit_line(linearized)?;
-    let params = delinearize(name, gain, offset, residuals);
+    let (gain, offset, _residuals) = linalg::fit_line(linearized)?;
+    let params = delinearize(name, gain, offset);
+    // Calculate delinearized residuals.
+    let residuals = calculate_residuals(name, params.clone(), data)?;
+    let params = Params {
+        residuals: Some(residuals),
+        ..params
+    };
     let rank = rank(name, params.clone());
 
     Ok(Complexity {
