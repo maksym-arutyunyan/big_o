ca5f1f30ba051b235ea525d6a56ded0a18c5a5d7
Maksym Arutyunyan <107496615+maksym-arutyunyan@users.noreply.github.com>
2025-07-15 08:45:42 +0200

    add run-all.sh

---
+++ run-all.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Define color codes
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Run cargo build and capture its exit status
+cargo build --release --all-targets
+build_status=$?
+
+# Run cargo test and capture its exit status
+cargo test
+test_status=$?
+
+# Run cargo test in release and capture its exit status
+cargo test --release
+test_release_status=$?
+
+# Run clippy and capture its exit status
+cargo clippy --all-targets -- -D warnings
+clippy_status=$?
+
+# Run fmt and capture its exit status
+cargo fmt --all -- --check
+fmt_status=$?
+
+# Run `keepsorted` only on files that are not ignored by `.gitignore`.
+# Also ignore `./misc/` and `./tests/`.
+git ls-files -co --exclude-standard \
+    | grep -vE "^misc/|^tests/|^README.md" \
+    | xargs -I {} bash -c "keepsorted '{}' --features gitignore,rust_derive_canonical" {}
+keepsorted_status=$?
+
+# Check if keepsorted changed any files.
+git diff --exit-code
+git_diff_status=$?
+
+# Check the status of each command and print the final status
+echo ""
+if [ $build_status -eq 0 ] &&\
+   [ $test_status -eq 0 ] &&\
+   [ $test_release_status -eq 0 ] &&\
+   [ $clippy_status -eq 0 ] &&\
+   [ $fmt_status -eq 0 ] &&\
+   [ $keepsorted_status -eq 0 ] &&\
+   [ $git_diff_status -eq 0 ] &&\
+   true; then
+    echo -e "All checks passed ${GREEN}ok${NC}."
+else
+    echo -e "Some checks ${RED}FAILED${NC}:"
+    if [ $build_status -ne 0 ]; then
+        echo -e " - cargo build ${RED}FAILED${NC}"
+    fi
+    if [ $test_status -ne 0 ]; then
+        echo -e " - cargo test ${RED}FAILED${NC}"
+    fi
+    if [ $test_release_status -ne 0 ]; then
+        echo -e " - cargo test --release ${RED}FAILED${NC}"
+    fi
+    if [ $clippy_status -ne 0 ]; then
+        echo -e " - clippy ${RED}FAILED${NC}"
+    fi
+    if [ $fmt_status -ne 0 ]; then
+        echo -e " - fmt ${RED}FAILED${NC}"
+    fi
+    if [ $keepsorted_status -ne 0 ]; then
+        echo -e " - keepsorted ${RED}FAILED${NC}"
+    fi
+    if [ $git_diff_status -ne 0 ]; then
+        echo -e " - git diff ${RED}FAILED${NC}"
+    fi
+fi
+
+# Exit with a status of 1 if any of the steps failed
+if [ $build_status -ne 0 ] ||\
+   [ $test_status -ne 0 ] ||\
+   [ $test_release_status -ne 0 ] ||\
+   [ $clippy_status -ne 0 ] ||\
+   [ $fmt_status -ne 0 ] ||\
+   [ $keepsorted_status -ne 0 ] ||\
+   [ $git_diff_status -ne 0 ] ||\
+   false; then
+    exit 1
+fi
